83e50fedce17dd053173c4e1d017ff36364bc4b5
dahyungwon <hyunda1016@naver.com>
2018-09-17 16:22:23 +0900

    move_up함수 완성

--- drills/drill05/drill05.py
+++ drills/drill05/drill05.py
@@ -2,25 +2,40 @@ from pico2d import *
 
 open_canvas()
 
-os.chdir('D:\\2D게임프로그래밍\\2018-2DGP\\Labs\Lecture03')
+os.chdir('D:\\2D게임프로그래밍\\제출\\2016180047\\drills\\drill05')
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
 def move_from_center_to_right():
-    pass
+    x, y = 800//2, 90
+    while x < 800-25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
+
 
 def move_up():
-    pass
+    x, y = 800 - 25, 90
+    while y < 600-50:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x += 2
+        delay(0.01)
+
 def move_left():
     pass
+
 def move_down():
     pass
 def move_from_left_to_center():
     pass
 
 def make_rectangle():
-    move_from_center_to_right()
+    #move_from_center_to_right()
     move_up()
     move_left()
     move_down()
