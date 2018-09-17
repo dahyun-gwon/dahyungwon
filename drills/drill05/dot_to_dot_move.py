f4d7b6084d7ab3bde4632268027b3ce76a304f6f
dahyungwon <hyunda1016@naver.com>
2018-09-17 19:12:17 +0900

    drill05 dot_to_dot 함수 선언
    일정한 위치로 가기위해 필요한 움직임
     함수를 선언하였다

---
+++ drills/drill05/dot_to_dot_move.py
@@ -0,0 +1,62 @@
+from pico2d import *
+
+open_canvas()
+os.chdir('D:\\2D게임프로그래밍\\2018-2DGP\\Labs\Lecture05')
+
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+def move_203_535():
+      pass
+
+def move_132_243():
+      pass
+
+def move_535_470():
+      pass
+
+def move_477_203():
+      pass
+
+def move_715_136():
+      pass
+
+def move_316_225():
+      pass
+
+def move_510_92():
+      pass
+
+def move_692_518():
+      pass
+
+def move_682_336():
+      pass
+
+def move_712_349():
+      pass
+
+
+
+
+while True:
+    move_203_535()
+    
+    move_132_243()
+    
+    move_535_470()
+    
+    move_477_203()
+    
+    move_715_136()
+    
+    move_316_225()
+    
+    move_510_92()
+    
+    move_692_518()
+    
+    move_682_336()
+    
+    move_712_349()
+      
