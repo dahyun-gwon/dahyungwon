8934b7d190c56047ccacdfa2fda0e743e8c51b5f
dahyungwon <hyunda1016@naver.com>
2018-09-17 16:09:23 +0900

    함수 완성
    실행하기위해 필요한 모든 함수 완성

--- drills/drill05/drill05.py
+++ drills/drill05/drill05.py
@@ -7,8 +7,24 @@ os.chdir('D:\\2D게임프로그래밍\\2018-2DGP\\Labs\Lecture03')
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def make_rectangle():
+def move_from_center_to_right():
     pass
+
+def move_up():
+    pass
+def move_left():
+    pass
+def move_down():
+    pass
+def move_from_left_to_center():
+    pass
+
+def make_rectangle():
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_from_left_to_center()
 
 def make_circle():
     pass
