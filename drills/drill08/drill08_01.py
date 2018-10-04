744a793ff31559d6450b4c16afc369931c42cebf
dahyungwon <hyunda1016@naver.com>
2018-10-04 17:08:09 +0900

    drill 07_1 p1->p2 함수구현
    p1에서 p2로 가기위한 함수를 만들었다

--- drills/drill08/drill08_01.py
+++ drills/drill08/drill08_01.py
@@ -47,7 +47,8 @@ def draw_big_point(p):
 
 
 def draw_point(p):
-  pass
+    turtle.goto(p)
+    turtle.dot(5, random.random(), random.random(), random.random())
 
 
 
@@ -57,7 +58,13 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     draw_big_point(p3)
     draw_big_point(p4)
     draw_big_point(p5)
-    pass
+
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+        draw_point((x, y))
+    draw_point(p2)
 
 
 
