2f3987cc11cad35a0f937037d2d086c0c22cebc0
dahyungwon <hyunda1016@naver.com>
2018-10-11 22:03:36 +0900

    pause_state변경
    일시중지시 버튼이 깜빡이며 화면이 화면이 그대로 출력되도록 변경

--- drills/drill10-2/pause_state.py
+++ drills/drill10-2/pause_state.py
@@ -1,15 +1,27 @@
 import game_framework
 from pico2d import *
 import main_state
-
-
+import title_state
 
 name = "PauseState"
 image = None
 
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause.png')
+        self.switch = 0
+
+    def draw(self):
+        if self.switch == 1:
+            self.image.draw(400, 300)
+
+    def update(self):
+        self.switch = (self.switch + 1) % 5
+        delay(0.05)
+
 def enter():
     global image
-    image = load_image('pause.png')
+    image = Pause()
 
 def exit():
     global image
@@ -20,7 +32,7 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            game_framework.quit()
+            game_framework.change_state(title_state)
 
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
@@ -28,19 +40,16 @@ def handle_events():
 
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+    main_state.boy.draw()
+    main_state.grass.draw()
+    image.draw()
     update_canvas()
 
-
 def update():
-    pass
-
+    image.update()
 
 def pause():
     pass
 
-
 def resume():
     pass
-
-
