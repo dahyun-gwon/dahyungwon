8794c20a48b44993041865d714637c75cc4bbe40
dahyungwon <hyunda1016@naver.com>
2018-10-11 21:09:50 +0900

    함수파일선언
    실습에 필요한 함수 파일들을 추가하였다

---
+++ drills/drill10/pause_state.py
@@ -0,0 +1,30 @@
+import game_framework
+from pico2d import *
+
+name = "PauseState"
+image = None
+def enter():
+    pass
+
+def exit():
+    pass
+
+
+def handle_events():
+    pass
+
+def draw():
+    pass
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
