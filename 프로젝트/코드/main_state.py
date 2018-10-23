f0421011cdcafeb5161d5caf965a7a0ef2e5cba9
dahyungwon <hyunda1016@naver.com>
2018-10-23 15:21:27 +0900

    불 정령 움직임 구현
    불정령이 캐릭터를 뒤따라 다니도록 함수 구현

--- 프로젝트/코드/main_state.py
+++ 프로젝트/코드/main_state.py
@@ -3,17 +3,19 @@ import game_framework
 
 open_canvas(1200,800)
 name = "MainState"
-
-
-
-
-
+x = 0
+y = 0
 
 class Girl:
+    global x
+    global y
     def __init__(self):
-        self.x, self.y = 200, 300
         self.image = load_image('tiena.png')
         self.x_dir,self.y_dir=0,0
+        self.x = 500
+        self.y = 500
+        x=self.x
+        y=self.y
 
     def hanlde_events(self):
         events = get_events()
@@ -21,119 +23,95 @@ class Girl:
             if event.type == SDL_KEYDOWN:
                 if event.key == SDLK_RIGHT:
                     self.x_dir += 1
-
-
                 elif event.key == SDLK_LEFT:
                     self.x_dir -= 1
-
-
                 elif event.key == SDLK_UP:
                     self.y_dir += 1
-
-
                 elif event.key == SDLK_DOWN:
                     self.y_dir -= 1
-
-
             elif event.type== SDL_KEYUP:
                 if event.key == SDLK_RIGHT:
                     self.x_dir -= 1
-
-
                 elif event.key == SDLK_LEFT:
                     self.x_dir += 1
-
-
                 elif event.key == SDLK_UP:
                     self.y_dir -= 1
-
-
                 elif event.key == SDLK_DOWN:
                     self.y_dir += 1
+
     def update(self):
-        global G_x
-        global G_y
         self.x+=self.x_dir*1
         self.y+=self.y_dir*1
-        G_x=self.x
-        G_y=self.y
 
 
     def draw(self):
-        self.image.draw(self.x,self.y)
+        self.image.draw(self.x, self.y)
+    def returnX(self):
+        return self.x
+    def returnY(self):
+        return self.y
 
 class Fire_Wisp:
+
     def __init__(self):
         self.image = load_image('fire_wisp.png')
-        self.x,self.y=G_x,G_y
+        self.x = girl.returnX()-30
+        self.y = girl.returnY()+50
+        self.x_dir=0
+        self.y_dir=0
+
+    def hanlde_events(self):
+        pass
+
+    def update(self):
+        self.x=girl.returnX()-30
+        self.y=girl.returnY()+50
 
     def draw(self):
-        self.image.draw(self.x,self.y)
-
-
+        self.image.draw(self.x, self.y)
 
 class Water_Wisp:
     def __init__(self):
         self.image = load_image('Watar_wisp.png')
-
     def draw(self):
-        self.image.draw(G_x,G_y)
-
+        pass
 
 class Universe:
     def __init__(self):
         self.image=load_image('universe.jpg')
-
     def draw(self):
         self.image.draw(600,400)
 
 universe=Universe()
 girl=Girl()
-fire_wisp=Fire_Wisp()
-water_wisp=Water_Wisp()
+fire=Fire_Wisp()
 
 def enter():
     pass
-
-
 def exit():
     pass
-
-
 def pause():
     pass
-
-
 def resume():
     pass
-
-
 def handle_events():
     girl.hanlde_events()
 
-
-
 def update():
     girl.update()
-
-
-
+    fire.update()
 
 def draw():
     universe.draw()
     girl.draw()
-    fire_wisp.draw()
-    water_wisp.draw()
-
+    fire.draw()
 
 while(True):
     clear_canvas()
 
-
     draw()
-    update_canvas()
-
     handle_events()
+    update_canvas()
     update()
 
 
