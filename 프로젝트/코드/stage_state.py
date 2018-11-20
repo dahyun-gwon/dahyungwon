77427013002ea417ec1a52b9667b4c0e90634e53
dahyungwon <hyunda1016@naver.com>
2018-11-20 22:35:15 +0900

    프로젝트_스테이지 창 구현

--- 프로젝트/코드/stage_state.py
+++ 프로젝트/코드/stage_state.py
@@ -1,46 +1,70 @@
 import game_framework
 from pico2d import *
-import start_state
-
+import main_state
 
 image = None
-logo_time = 0.0
 
 
 def enter():
     global image
+    global stage_ui
+    global frame
+    global cnt
+    global mouse_x
+    global mouse_y
+    mouse_x=0
+    mouse_y=0
     image = load_image('stage_background.jpg')
-
+    stage_ui=load_image('stage_ui.png')
+    frame=0
+    cnt=0
 
 def exit():
     global image
-    del (image)
-
-
-def update():
-    global logo_time
-    if (logo_time > 1.0):
-        logo_time = 0
-        game_framework.push_state(start_state)
-    delay(0.01)
-    logo_time += 0.01
-
-
-def draw():
-    global image
-    clear_canvas()
-    image.draw(400, 300)
-    update_canvas()
-
-
+    del(image)
 
 
 def handle_events():
     events = get_events()
-    pass
+    global mouse_x
+    global mouse_y
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type==SDL_MOUSEMOTION:
+            mouse_x=event.x
+            mouse_y=799-event.y
+
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+
+            elif event.type == SDL_MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.x, event.y
+                print("%d %d",mouse_x,mouse_y)
+                if mouse_x>=150 and mouse_x<=250 and mouse_y>=350 and mouse_y<=450:
+                    game_framework.change_state(main_state)
+def update():
+    global cnt
+    global frame
+    global mouse_x
+    global mouse_y
+    if (cnt > 5):
+        cnt = 0
+        frame += 1
+    cnt += 1
+    frame =frame % 10
+    delay(0.01)
+
+def draw():
+    clear_canvas()
+    stage_ui.opacify(0.8)
+    image.draw(600, 400)
+    stage_ui.clip_draw(100*frame,0,100,100,200,400)
+    stage_ui.clip_draw(100 * frame, 0, 100, 100, 400, 400)
+    stage_ui.clip_draw(100 * frame, 0, 100, 100, 600, 400)
+    stage_ui.clip_draw(100 * frame, 0, 100, 100, 800, 400)
+    stage_ui.clip_draw(100 * frame, 0, 100, 100, 1000, 400)
 
 
-def pause(): pass
-
-
-def resume(): pass
+    update_canvas()
--- 프로젝트/코드/title_state.py
+++ 프로젝트/코드/title_state.py
@@ -1,7 +1,7 @@
 import game_framework
 from pico2d import *
+import stage_state
 import main_state
-
 name = "TitleState"
 image = None
 
@@ -27,7 +27,7 @@ def handle_events():
                 game_framework.quit()
 
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(main_state)
+                game_framework.push_state(stage_state)
 
 def draw():
     clear_canvas()
